c51b6da6d9094eae5023b306074cfb8bf6c19d10
tephro <tephro@icloud.com>
2019-04-05 15:47:02 +0300

    build.sh update

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 go run ${scriptDir}/sdk/normalization/gen/*.go -out ${scriptDir}/sdk/normalization/event_functions.go || exit 1
 gofmt -w ${scriptDir}/sdk/normalization/event_functions.go
 go test ./...
-go build -o /usr/local/bin/raccoon ${scriptDir}/main.go
+go build -mod vendor -o /usr/local/bin/raccoon ${scriptDir}/main.go
